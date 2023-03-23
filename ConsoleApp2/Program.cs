427789ccebc3e9292ec390af52e1aa3933772c5b
iloveu777 <zuxplil1337@yandex.ru>
2023-03-23 12:21:06 +0200

    AS

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,21 +11,31 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //сортировка n по возрастанием
-            int[] array = { -5, 20, -10, 2, 8 };
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        int p = array[j]; array[i] = array[j]; array[j] = p;
-                    }
-                }
-            }
+            //int[] array = { -5, 20, -10, 2, 8 };
+            //for (int i = 0; i < array.Length - 1; i++)
+            //{
+            //    for (int j = i + 1; j < array.Length; j++)
+            //    {
+            //        if (array[i] > array[j])
+            //        {
+            //            int p = array[j]; array[i] = array[j]; array[j] = p;
+            //        }
+            //    }
+            //}
+            //foreach (int elem in array)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+
+            int[] array = { 1, -2, 3, 4, -5, 6, -7, 8, 9, 10, -11, };
             foreach (int elem in array)
-            {
-                Console.WriteLine($"{elem}");
-            }
+                {
+                Console.WriteLine();
+                   Console.WriteLine($"{elem}");
+                }
+            Array.Sort( array );
+            
+            Array.Reverse( array );
             Console.ReadLine();
         }
     }
